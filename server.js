7d98067c418a8e63fa6237b1f77dcb1c6fbe80dc
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-05-22 14:34:17 +0530

    create a basic setup of backend

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
 
+//rest object
 const app = express();
 
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("short"));
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//route
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.status(201).send("<h1>Welcome to Food Server</h1>");
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is Running ${PORT}`);
 });
