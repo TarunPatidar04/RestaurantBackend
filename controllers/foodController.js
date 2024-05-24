85b8149a4e798d35792e031239fc63fa15fea9a7
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-05-24 17:59:03 +0530

    create a  getAll food by Id and Single and Based on Restaurant Id

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -16,7 +16,7 @@ const createFoodController = async (req, res) => {
     } = req.body;
     //validation
     if (!title || !description || !price || !restaurant) {
-      return res.status(500).send({
+      return res.status(404).send({
         success: false,
         message: "Please Provide all fields",
       });
@@ -41,7 +41,8 @@ const createFoodController = async (req, res) => {
       message: "New Food Created Successfully",
       newFood,
     });
-  } catch {
+  } catch (error) {
+    console.log(error);
     return res.status(500).send({
       success: false,
       message: "Error in create food API",
@@ -50,6 +51,105 @@ const createFoodController = async (req, res) => {
   }
 };
 
+// GET ALL FOODS
+const getAllFoodController = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    if (!foods) {
+      return res.status(404).send({
+        success: false,
+        message: "Foods not found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "All Foods",
+      totalFoods: foods.length,
+      foods,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in get All food API",
+      error,
+    });
+  }
+};
+
+// GET SINGLE FOODS BY ID
+const getSingleFoodController = async (req, res) => {
+  try {
+    const id = req.params.id;
+    //validation
+    if (!id) {
+      return res.status(404).send({
+        success: false,
+        message: "Please Provide Id",
+      });
+    }
+    //find food
+    const food = await foodModel.findById(id);
+    if (!food) {
+      return res.status(404).send({
+        success: false,
+        message: "Food not found with this Id",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Food successfully found",
+      food,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Single food API",
+      error,
+    });
+  }
+};
+
+// GET FOOD BY Restaurant
+const getFoodByRestaurantController = async (req, res) => {
+  try {
+    const restaurantId = req.params.id;
+    //validation
+    if (!restaurantId) {
+      return res.status(404).send({
+        success: false,
+        message: "Please Provide Id",
+      });
+    }
+    //find food
+    const food = await foodModel.find({
+      restaurant: restaurantId,
+    });
+    if (!food) {
+      return res.status(404).send({
+        success: false,
+        message: "Food not found with this Id",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Food base on Restaurant",
+      food,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Single food API",
+      error,
+    });
+  }
+};
+
 module.exports = {
   createFoodController,
+  getAllFoodController,
+  getSingleFoodController,
+  getFoodByRestaurantController,
 };
--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
-const { createFoodController } = require("../controllers/foodController");
+const {
+  createFoodController,
+  getAllFoodController,
+  getSingleFoodController,
+  getFoodByRestaurantController,
+} = require("../controllers/foodController");
 
 const router = express.Router();
 
 //route
 //CREATE FOOD
-router.post("/create",authMiddleware, createFoodController);
+router.post("/create", authMiddleware, createFoodController);
+
+//get ALL FOOD
+router.get("/getAll", getAllFoodController);
+
+//get SINGLE FOOD
+router.get("/get/:id", getSingleFoodController);
+
+//get food by Gestorant
+router.get("/getbyRestaurant/:id", getFoodByRestaurantController);
+
 module.exports = router;
